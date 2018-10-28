f2fd2551379f4b1a4eb8bc9f0f9726f1a0fc707f
Lauren Nix <lcarrnix@users.noreply.github.com>
2018-10-28 15:27:46 -0400

    Updated welcome message

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        Label mikeLabel = new Label("Hi this is our trivia game.");
+        Label mikeLabel = new Label("Welcome to our trivia game!");
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.BASELINE_CENTER);
         layout.getChildren().addAll(mikeLabel);
