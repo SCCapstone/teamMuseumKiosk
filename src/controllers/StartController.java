de9fc4aeecb6d0083426ddf5a2f65ec25f4a1310
Lauren Nix <lcarrnix@users.noreply.github.com>
2018-11-17 15:39:46 -0500

    Update to include Jacob's changes

--- src/controllers/StartController.java
+++ src/controllers/StartController.java
@@ -12,30 +12,51 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import main.User;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StartController implements Initializable {
     @FXML
-    private TextField name;
+    private TextField name, email;
+    
+    @FXML
+	private Text missingInfoText;
 
     public StartController(){}
 
     public void buttonClick(ActionEvent actionEvent) throws IOException {
-        User newUser = new User(name.getText(), 0);
+        if(name.getText().trim().isEmpty() || email.getText().trim().isEmpty()){
+	            missingInfoText.setText("Please enter your initials and email");
+	            return;
+	    }
+
+        if(name.getText().trim().length() > 3) {
+	            missingInfoText.setText("Initials cannot be longer than 3 characters");
+	            return;
+	    }
+
+
+	    //TODO: verify email is in an appropriate format. May use regex for this.
+	    collectEmail(email.getText().trim());
+
+        User newUser = new User(name.getText().trim(), 0, email.getText().trim());
 
         Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/design/question.fxml"));
-        Parent root = loader.load();
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/design/question.fxml"));
+	    Parent root = loader.load();
 
         QuestionController controller = loader.getController();
-        controller.setUser(newUser);
-        loader.setController(controller);
+	    controller.setUser(newUser);
+	    loader.setController(controller);
 
-        Scene scene = new Scene(root, 600, 1000);
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(root, 600, 550);
+	    stage.setScene(scene);
+	    stage.show();
     }
 
     public void goToAdminPage(ActionEvent actionEvent) throws IOException {
@@ -50,6 +71,25 @@ public class StartController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+    
+    //Creates a buffered writer to append email to csv file
+	private void collectEmail(String email) {
+	    BufferedWriter writer = null;
+
+        try {
+	        writer = new BufferedWriter(new FileWriter("EmailList.csv", true));
+	        writer.write(email);
+	        writer.newLine();
+	        writer.flush();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (writer != null) try {
+	           writer.close();
+	    } catch (IOException e) {
+	  }
+	}
+}
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
