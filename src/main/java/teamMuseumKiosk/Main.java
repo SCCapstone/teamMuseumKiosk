80904f9af99aee33228105e7b00bf6c001b291de
Michael Cantwell <macsc97@yahoo.com>
2019-04-21 18:00:54 -0400

    large rework of difficulty, question list
    -Added capability to have commas in the questions and answers
    -properly formatted the questions sent by museum and added to the list
    -reworked difficulty system to randomly increase/keep same difficulty on correct answer, while wrong answers go down in difficulty, and questions are chosen randomly now from the appropriate difficulty
    -removed difficulty of "4" due to museum not submitting any questions with that difficulty
    -added basis for file structure? (its commented out in the main function, but it wont work on a mac)

--- src/main/java/teamMuseumKiosk/Main.java
+++ src/main/java/teamMuseumKiosk/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
 import java.net.URL;
 
 public class Main extends Application {
@@ -16,6 +17,23 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //This creates a directory on the C drive where resources can go
+        //this might break mac's
+
+        /*File tempFile = new File("C://MuseumTriviaGame/");
+        boolean exists = tempFile.exists();
+        if (!exists)
+        {
+            File dir = new File("C://MuseumTriviaGame/");
+            dir.mkdir();
+            File img = new File("C://MuseumTriviaGame/Images/");
+            img.mkdir();
+            File ads = new File("C://MuseumTriviaGame/Images/Advertisements/");
+            ads.mkdir();
+            File vid = new File("C://MuseumTriviaGame/Videos/");
+            vid.mkdir();
+        }*/
+
         URL url = new URL(getClass().getResource("/design/splashScreen.fxml").toExternalForm());
         FXMLLoader loader = new FXMLLoader(url);
         Parent root = loader.load();
