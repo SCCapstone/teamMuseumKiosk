cefba688dc17ab5e05b7b49e2da7c5e463de48a5
Michael Cantwell <macsc97@yahoo.com>
2019-04-24 19:59:46 -0400

    questions now appear in questions and can be added correctly by question adder

--- src/main/java/controllers/AddQuestionController.java
+++ src/main/java/controllers/AddQuestionController.java
@@ -28,6 +28,7 @@ import java.net.URL;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -204,7 +205,11 @@ public class AddQuestionController implements Initializable {
                 csvString.append(i);
                 csvString.append(",");
             }
-            csvString.append("/images/"+filePath.getName());
+            csvString.append("./images/"+filePath.getName());
+            //actually move file there now
+            //Path dest = Paths.get("./images/" + filePath.getName());
+            //Path src = Paths.get()
+            //Files.copy(src,dest);
 
         }
 
@@ -229,7 +234,7 @@ public class AddQuestionController implements Initializable {
 
         try {
             //TODO put in the images folder
-            File path = new File("src/main/resources/images/"+filePath.getName());
+            File path = new File("./images/"+filePath.getName());
             //Path path = FileSystems.getDefault().getPath(question.getText()+".jpg");
             Files.copy(filePath.toPath(),path.toPath());
         }catch (Exception e){
